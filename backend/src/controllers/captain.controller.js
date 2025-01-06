af71ef8d2283c84b9c8e3f8fcbfd1321d7e42d7f
HaseethVardhan <haseethvardhan01@gmail.com>
2025-01-06 21:37:19 +0530

    Add captain login, profile retrieval, and logout functionality with token management

--- backend/src/controllers/captain.controller.js
+++ backend/src/controllers/captain.controller.js
@@ -3,6 +3,7 @@ import {asyncHandler} from '../utils/asyncHandler.js'
 import { validationResult } from 'express-validator'
 import ApiError from '../utils/ApiError.js'
 import ApiResponse from '../utils/ApiResponse.js'
+import blackListToken from '../models/blackListToken.model.js'
 
 const registerCaptain = asyncHandler( async (req, res)=> {
     const errors = validationResult(req)
@@ -51,4 +52,60 @@ return res
 .json(new ApiResponse(200, {newCaptain, token}, 'Captain registered'))
 })
 
-export {registerCaptain}
+const loginCaptain = asyncHandler( async(req, res) => {
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty()){
+        throw new ApiError(400, 'Express validation error')
+    }
+
+    const {email, password} = req.body
+
+    if(!email || !password){
+        throw new ApiError(400, 'Enter email and password')
+    }
+
+    const existCaptain = await Captain.findOne({email}).select('+password')
+
+    if(!existCaptain){
+        throw new ApiError(400, 'No such user exists')
+    }
+
+    const passwordCheck = await existCaptain.comparePassword(password)
+
+    if(!passwordCheck){
+        throw new ApiError(400, 'Wrong password')
+    }
+
+    const captain = await Captain.findOne({email})
+    const token = await captain.generateAuthToken()
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(201)
+    .cookie('token', token)
+    .json(new ApiResponse(201, {captain, token}, 'Captain logged in'))
+})
+
+const getCaptainProfile = asyncHandler( async( req, res) => {
+    return res
+    .status(200)
+    .json(new ApiResponse(200, req.captain, 'Profile fetched successfully'))
+})
+
+const logoutCaptain = asyncHandler( async(req, res) => {
+    const token = req.cookies.token || req.headers.authorization.split(' ')[ 1 ]
+
+    const blackList = await blackListToken.create({token})
+
+    return res
+    .status(200)
+    .clearCookie('token')
+    .json(new ApiResponse(200, {}, 'captain logged out'))
+})
+
+export {registerCaptain, loginCaptain, getCaptainProfile, logoutCaptain}
--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -4,6 +4,7 @@ import jwt from 'jsonwebtoken'
 import { asyncHandler } from '../utils/asyncHandler.js'
 import ApiError from '../utils/ApiError.js'
 import blackListToken from '../models/blackListToken.model.js'
+import { Captain } from '../models/captain.model.js'
 
 const verifyUser = asyncHandler( async (req, res, next) => {
     const token = req.cookies.token || req.headers.authorization?.split(' ')[ 1 ]
@@ -36,4 +37,35 @@ const verifyUser = asyncHandler( async (req, res, next) => {
 })
 
 
-export {verifyUser}
+const verifyCaptain = asyncHandler( async (req, res, next) => {
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[ 1 ]
+
+    const isBlackListToken = await blackListToken.findOne({token})
+
+    if(isBlackListToken){
+        throw new ApiError(400, 'token deleted or unauthorized access')
+    }
+
+    if(!token){
+        throw new ApiError(400, 'unauthorized request')
+    }
+
+    
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const captain = await Captain.findById(decoded._id)
+
+        req.captain = captain
+
+        return next()
+        
+    } catch (error) {
+        throw new ApiError(400, 'unauthorized')
+    }
+    
+
+
+})
+
+
+export {verifyUser, verifyCaptain}
